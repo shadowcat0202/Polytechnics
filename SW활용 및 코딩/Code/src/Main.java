57dc5cf91a8f66d55426298a964f2a4e15090695
shadowcat0202 <fxlimsin@gmail.com>
2022-03-17 11:59:09 +0900

    수업시간

--- SW활용 및 코딩/Code/src/Main.java
+++ SW활용 및 코딩/Code/src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static int sum(int i, int j){
 
+        System.out.println("변경전 =" + System.identityHashCode(i));
+        System.out.println("함수 내에서의 i =" + ++i);
+        System.out.println("변경전 ="+ System.identityHashCode(i));
+        return i + j;
+    }
+
+    public static void main(String[] args){
+        int i, j;
+        char a;
+        String b;
+        final int TEN = 10;
+        i = 1;
+        System.out.println(System.identityHashCode(i));
+        j = sum(i, TEN);
+        System.out.println("함수 밖에서 i = " + i);
+        a = '?';
+        b = "Hello";
+        System.out.println(System.identityHashCode(i));
+
+        System.out.println(b);
+        System.out.println(TEN);
+        System.out.println(j);
     }
 }
