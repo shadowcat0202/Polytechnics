5ed1dba8dbce212c8430f6ebef2da9872d04d778
shadowcat0202 <fxlimsin@gmail.com>
2022-04-11 11:02:35 +0900

    new feat open API

--- AI기초프로그래밍/testPorject/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('https://archive.ics.uci.edu/ml/' 'machine-learning-databases/iris/iris.data', header=None)
-print(df.head())
-
-sepal_l = df.iloc[0:100, 0]
-sepal_w = df.iloc[0:100, 1]
-petal_l = df.iloc[0:100, 2]
-petal_w = df.iloc[0:100, 3]
