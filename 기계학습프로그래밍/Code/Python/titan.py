b8ce1535c3fe9464e9f75068916b858f946b2a55
shadowcat0202 <fxlimsin@gmail.com>
2022-04-17 13:38:11 +0900

    feat kfold_depth check fail

--- 기계학습프로그래밍/Code/Python/titan.py
+++ 기계학습프로그래밍/Code/Python/titan.py
@@ -1,140 +1,154 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn
 import numpy as np
+import re  # 비정규식
+import seaborn as sns
 
-df_train = pd.read_csv('./dataset/titanic/train.csv')
-df_train = df_train.drop(["Name", "PassengerId", "Ticket", "Fare", "Cabin"], axis=1)
-df_train["Age"].fillna(df_train["Age"].mean(), inplace=True)
-df_train["Embarked"] = df_train["Embarked"].fillna("S")  # 가장 많은 S로 통일
-df_train["Sex"] = df_train["Sex"].map({"male": 0, "female": 1})
-df_train["Embarked"] = df_train["Embarked"].map({"Q": 0, "C": 1, "S": 2})
+train_df = pd.read_csv("./dataset/titanic/train.csv")
+test_df = pd.read_csv("./dataset/titanic/test.csv")
 
-# print(df_train.isna().sum())
-from sklearn.ensemble import RandomForestClassifier
+PassengerId = test_df["PassengerId"]
+# print(train_df.head())
 
-# classifire = DecisionTreeClassifier()
-classifire = RandomForestClassifier(max_depth=4, n_estimators=300)
-# classifire = LinearSVC()
+# print([(cab, type(cab)) for cab in train_df.Cabin.unique()])
 
-X = df_train.drop(["Survived"], axis=1)
-y = df_train["Survived"]
+original_train = train_df.copy()
 
-classifire.fit(X, y)
-print("학습 정확도:", round(classifire.score(X, y), 2))
-
-df_test = pd.read_csv('./dataset/titanic/test.csv')
-pid = df_test["PassengerId"]  # [!!!]submit.csv 를 만들어줄 때 PassengerId가 필요하기 때문에 drop하기 전에 저장해둔다
-# predict하기 위한 test data도 columns를 동일하게 맞춰주어야 한다
-df_test = df_test.drop(["Name", "PassengerId", "Ticket", "Fare", "Cabin"], axis=1)
-df_test["Age"].fillna(df_test["Age"].mean(), inplace=True)
-df_test["Embarked"] = df_test["Embarked"].fillna("S")
-df_test["Sex"] = df_test["Sex"].map({"male": 0, "female": 1})
-df_test["Embarked"] = df_test["Embarked"].map({"Q": 0, "C": 1, "S": 2})
-
-test_result = classifire.predict(df_test)
-
-submit = pd.DataFrame({"PassengerId": pid, "Survived": test_result})
-# index=False를 안하면 csv파일 0번쨰 columns에 index를 붙여준다 즉 제출용에 맞게 설정해주어야 한다
-submit.to_csv("./dataset/titanic/submit.csv", index=False)
-
-hit = 0
-miss = 0
-# test_gt = pd.read_csv("./dataset/titanic/wow.csv")
-base = pd.read_csv("./dataset/titanic/submit.csv")
-#
-# for i in range(len(test_result)):
-#     if base["Survived"][i] == test_gt["Survived"][i]:
-#         hit += 1
-#     else:
-#         miss += 1
-
-print(hit, miss, round(hit / (hit + miss), 4))
+full_data = [train_df, test_df]
 
 
-def myPredict(x):
-    if x["Sex"] == "male":
-        return 0
-    else:
-        return 1
+def get_title(name):
+    title_search = re.search(" ([A-Za-z]+)\.", name)
+    if title_search:
+        return title_search.group(1)
+    return ""
 
 
+Sex_mapping = {"female": 0, "male": 1}
+title_mapping = {"Mr": 1, "Master": 2, "Mrs": 3, "Miss": 4, "Rare": 5}
+Embarked_mapping = {'S': 0, 'C': 1, 'Q': 2}
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
 
-# pyplot 그려주기 위해서 replace작업==================================================================
-df_train["Pclass"] = df_train["Pclass"] \
-    .replace(1, "1st") \
-    .replace(2, "2nd") \
-    .replace(3, "3th")
-df_train["Survived"] = df_train["Survived"] \
-    .replace(1, "Alive") \
-    .replace(0, "Dead")
+for dataset in full_data:
+    # Cabin nan데이터 변경
+    dataset["Has_Cabin"] = dataset["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 
-for i in range(len(df_train)):
-    df_train.loc[i, "Age"] = int(df_train.loc[i, "Age"] / 10)
-df_train["Age"] = df_train["Age"].map({0: "0~9", 1: "10~19", 2: "20~29",
-                                       3: "30~39", 4: "40~49", 5: "50~59",
-                                       6: "60~69", 7: "Old", 8: "Old"})
+    # 가족 인원수 정확하게 파악
+    dataset["FamilySize"] = dataset["SibSp"] + dataset["Parch"] + 1  # 총 가족 인원수
 
+    # 혼자 or 단체에 따른 생존율이 다르므로 단독 여부 체크
+    dataset["IsAlone"] = 0  # 일단 빈거 만들고
+    dataset.loc[dataset["FamilySize"] == 1, "IsAlone"] = 1
 
-def showCountPlot():
-    seaborn.set_style(style="darkgrid")
-    feat = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-    i = 1
-    for f in feat:
-        plt.subplot(2, 4, i)
-        i += 1
-        seaborn.countplot(data=df_train, x=f, hue="Survived")
+    # Embarked에서 nan을 가장 많이 존재하는 data로 채우기
+    dataset["Embarked"] = dataset["Embarked"].fillna(
+        str(dataset["Embarked"].value_counts().to_frame().iloc[0].name)
+    )
 
-    plt.show(block=True)
+    age_avg = dataset["Age"].mean()  # 평균
+    age_std = dataset["Age"].std()  # 표준편차
+    age_null_count = dataset["Age"].isna().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset.loc[np.isnan(dataset['Age']), 'Age'] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
 
+    # Passenger Name으로 새로운 칼럼 생성
+    dataset["Title"] = dataset["Name"].apply(get_title)
+    dataset['Title'] = dataset['Title'].replace(
+        ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
-# showCountPlot()
+    # Mapping Sex
+    dataset["Sex"] = dataset["Sex"].map(Sex_mapping).astype(int)
 
+    # Mapping Title
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
 
-def show_survive_rat():
-    feat = ["Pclass", "Sex", "SibSp", "Parch", "Embarked"]
-    df_survive = df_train.loc[df_train["Survived"] == "Alive"]
-    df_dead = df_train.loc[df_train["Survived"] == "Dead"]
-    i = 1
-    for f in feat:
-        plt.subplot(2, 3, i)
-        i += 1
-        sur_info = df_survive[f].value_counts(sort=False)
+    # Mapping Embarked
+    dataset["Embarked"] = dataset["Embarked"].map(Embarked_mapping).astype(int)
+    # Fare은 중앙값으로 대체
+    dataset["Fare"] = dataset["Fare"].fillna(dataset["Fare"].median())
+    # Mapping Fare
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
 
-        category = sur_info.index
-        plt.title("Survival Rate in Total ({0})".format(f))
-        plt.pie(sur_info, labels=category, autopct="%0.1f%%")
+    # Mapping Age
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
 
-    plt.show(block=True)
+train_df = train_df.drop(drop_elements, axis=1)
+test_df = test_df.drop(drop_elements, axis=1)
 
+from sklearn.model_selection import KFold
 
-# show_survive_rat()
+cv = KFold(n_splits=10)
+accuracies = list()
+max_attrivutes = len(list(test_df))
+depth_range = range(1, max_attrivutes + 1)  # 1~속성개수 + 1 만큼 왜?
 
+from sklearn.tree import DecisionTreeClassifier
 
-def show_group_rate(feature):
-    df_survive = df_train.loc[df_train["Survived"] == "Alive"]
-    df_dead = df_train.loc[df_train["Survived"] == "Dead"]
+for depth in depth_range:
+    # print("depth:", depth, "==========================================")
+    fold_accuracy = []
+    tree_clf = DecisionTreeClassifier(max_depth=depth)
+    # print("Current max depth: ", depth, "\n")
+    for train_fold, valid_fold in cv.split(train_df):  # row index가 반환된다
+        f_train = train_df.loc[train_fold]  # Extract train data with cv indices
+        f_valid = train_df.loc[valid_fold]  # Extract valid data with cv indices
 
-    sur_info = df_survive[feature].value_counts(sort=False)
-    dead_info = df_dead[feature].value_counts(sort=False)
+        model = tree_clf.fit(X=f_train.drop(['Survived'], axis=1),
+                             y=f_train["Survived"])  # We fit the model with the fold train data
+        valid_acc = model.score(X=f_valid.drop(['Survived'], axis=1),
+                                y=f_valid["Survived"])  # We calculate accuracy with the fold validation data
+        fold_accuracy.append(valid_acc)
+        # print(valid_acc)
 
-    fig = plt.figure()
-    plt.title("Survival rete of " + feature)
-    for i, index in enumerate(sur_info.index):  # 리스트 요소를 꺼내는것과 동시에 인덱스도 같이 돌린다
-        # fig.add_subplot(len(sur_info)//4, len(sur_info) // 2, i + 1)  # Age 그래프 화면의 어느 위치
-        # 생존자와 사망자의 해당 클래스로 파이 그래프를 그리겠다
-        fig.add_subplot(1, len(sur_info.index), i + 1)  # Pclass
-        plt.pie([sur_info[index], dead_info[index]],
-                labels=["Survived", "Dead"],
-                autopct="%0.1f%%")
-        plt.title(str(index))
+    avg = sum(fold_accuracy) / len(fold_accuracy)
+    accuracies.append(avg)
+    # print("Accuracy per fold: ", fold_accuracy, "\n")
+    # print("Average accuracy: ", avg)
+    # print("\n")
 
-    print("survive:")
-    print(sur_info)
-    print("dead:")
-    print(dead_info)
-    plt.show()
+# Just to show results conveniently
+df = pd.DataFrame({"Depth": depth_range, "Average_Accuracy": accuracies})
+df = df[["Depth", "Average_Accuracy"]]
+print(df.to_string(index=False))
 
-# show_group_rate("Pclass")
-# show_group_rate("Sex")
-# show_group_rate("Age")
+y_train = train_df["Survived"]
+x_train = train_df.drop(["Survived"], axis=1).values
+x_test = test_df.values
+
+# 위에서 결정트리 depth = 3일때 정확도가 가장 높게 나왔으므로 3으로 설정하고 학습 시킨다
+tree_clf = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=42)
+tree_clf.fit(x_train, y_train)
+
+y_pred = tree_clf.predict(x_test)
+
+def score(comp_df):
+    perfect_df = pd.read_csv("./dataset/titanic/perfect.csv")
+    hit = 0
+    miss = 0
+    for i in range(len(comp_df)):
+        if comp_df["Survived"][i] == perfect_df["Survived"][i]:
+            hit += 1
+        else:
+            miss += 1
+    # print(hit, miss)
+    return round(hit / (hit + miss), 4)
+
+submission = pd.DataFrame({
+    "PassengerId": PassengerId,
+    "Survived": y_pred
+})
+submission.to_csv("out.csv",index=False)
+print("logistic", score(submission))
+
