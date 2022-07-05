a7276881a11299b6974f02367982e0163ca6e018
shadowcat0202 <fxlimsin@gmail.com>
2022-07-05 17:47:50 +0900

    feat final exam

--- AI기초프로그래밍/Code/T.py
+++ AI기초프로그래밍/Code/T.py
@@ -1,7 +1,28 @@
 import numpy as np
 import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+def draw_bar(Data_Frame, column_name):
+    header_name_list = Data_Frame.columns.tolist()
+    unique_count = Data_Frame[column_name].value_counts()
+    index_list = unique_count.index.tolist()
+    # print(unique_count, index_list)
+    plt.bar(index_list, unique_count)
+    plt.xlabel(f"{column_name}", fontsize=18)
+    plt.ylabel(f"count", fontsize=18)
+    plt.show()
 
-t = np.array([0.,1.,2.,3,4.,5.])
-print(t.shape)
-t = torch.FloatTensor(t)
-print(t)
+def pokemon():
+    original_data = pd.read_csv("dataset/pokemon/Pokemon.csv", sep=',', index_col=0)
+    print(original_data)
+
+    original_data.info()
+
+    draw_bar(original_data, "Generation")
+
+
+x_data = [[1, 2], [2, 3], [3, 1],
+          [4, 3], [5, 3], [6, 2]]
+y_data = [[0], [0], [0], [1], [1], [1]]
+
+
---
+++ 학습데이터처리/Code/Pokemon.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+original_data = pd.read_csv("dataset/pokemon/Pokemon.csv")
+print(type(original_data))
+# original_data.head()
+
+# original_data.info()
+
+# print(original_data.describe())
